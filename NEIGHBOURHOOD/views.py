d5faa24261f8688e8e72d13be9f552bc278b9e77
peter max <petermax2004@gmail.com>
2019-08-09 12:22:48 +0300

    Add the user_profile function to allow users to view their profiles after they have logged in

--- NEIGHBOURHOOD/views.py
+++ NEIGHBOURHOOD/views.py
@@ -13,3 +13,9 @@ def neighborhood(request,neighborhood_id):
     neighborhood=Neighborhood.objects.filter(id=neighborhood_id)
     results=Business.objects.filter(neighborhood=neighborhood_id)
     return render(request,'neighborhood.html',{"neighborhood":neighborhood,"results":results})
+
+@login_required(login_url='/accounts/login')
+def user_profile(request):
+    current_user = request.user
+    profile=Profile.objects.filter(user=current_user)
+    return render(request,'profile.html',{"profile":profile})
