95e705280f83ffec1d53696282afbbdd945580ea
peter max <petermax2004@gmail.com>
2019-08-09 10:48:11 +0300

    In the models.py add the Neighborhood class with fields for name,location,image of the neighborhood,occupats in the neighborhood,and admin as a foreign key

--- NEIGHBOURHOOD/models.py
+++ NEIGHBOURHOOD/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name=models.Charfield(max_length=30)
+    location=models.CharField(max_length=30)
+    neighborhood_image=models.ImageField(upload_to='neighborhood/')
+    occupants=models.IntegerField()
+    admin=models.ForeignKey(User,on_delete=models.CASCADE)
