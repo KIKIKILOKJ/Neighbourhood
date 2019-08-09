6a2e076999bb665f375eaef4a491d6374b9b9c5b
peter max <petermax2004@gmail.com>
2019-08-09 12:01:28 +0300

    In the views.py file import them models classes and decorator login required then add the index function to provide the response for the the page the user sees when the page loads

--- NEIGHBOURHOOD/views.py
+++ NEIGHBOURHOOD/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Neighborhood,Profile,Business
 
 # Create your views here.
+def index(request):
+    neighborhood=Neighborhood.objects.all()
+    title='The Neighborhood'
+    return render(request,'index.html',{"title":title,"neighborhood":neighborhood})
